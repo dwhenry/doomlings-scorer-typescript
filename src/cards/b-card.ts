f230d5fdc125ba727d4537b74345e222b1c2f697
David Henry <david@decoybecoy.com>
2023-09-05 09:56:10 +0100

    Enforce data requirements at object creation

--- src/cards/b-card.ts
+++ src/cards/b-card.ts
@@ -7,7 +7,7 @@ addBasicCard('BEAUTY', 'green', 'Classic', 2);
 addBasicCard('BIG EARS', 'green', 'Classic', 2);
 addBasicCard('BINARY', 'colourless', 'Techlings', 0);
 
-const bionic_arm: Card = {
+const bionic_arm: PlayerCard = {
   name: 'BIONIC ARM',
   type: 'red',
   pack: 'Techlings',
@@ -28,6 +28,34 @@ const bionic_arm: Card = {
     const multiplier = 1;
 
     inst.finalB = techlingCards.length * multiplier;
+  }
+};
+addCard(bionic_arm);
+
+addBasicCard('BLOOM', 'multi-colour', 'Classic', 1);
+addBasicCard('BLUBBER', 'blue', 'Classic', 4);
+addBasicCard('BONE REINFORCEMENT', 'red', 'Techlings', 4);
+addBasicCard('BONES', 'colourless', 'Classic', 2);
+addBasicCard('BONY PLATES', 'green', 'Dinolings', 2);
+
+const boredom: PlayerCard = {
+  name: 'BOREDOM',
+  type: 'colourless',
+  pack: 'Classic',
+  calcA: (inst: CardInstance): void => {
+    inst.finalA = 0;
+  },
+  calcB: (
+    inst: CardInstance,
+    allPlayerCards: Array<Array<CardInstance>>,
+    currentPlayer: number
+  ): void => {
+    const playerCards = allPlayerCards[currentPlayer];
+    const effectCards = playerCards.filter(
+      (inst) => inst.card.effect !== undefined
+    );
+
+    inst.finalB = effectCards.length;
   }
 };
 addCard(bionic_arm);
