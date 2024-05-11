865fcf20ebebbf28cbe0b4c984944aa870080e29
Arran France <arran@arranfrance.com>
2024-05-11 19:04:47 +0100

    refactor:
    * cards can now have multiple colours
    * multi-coloured is replaced with the correct colorus
    * the cards I did badly now do a and b scoring properly

--- src/cards/catastophe.ts
+++ src/cards/catastophe.ts
@@ -3,13 +3,13 @@ import { addCard } from '../cardContainer';
 
 const aiTakeover: CatastopheCard = {
   name: 'AI TAKEOVER',
-  type: 'catastrophe',
+  type: ['catastrophe'],
   pack: 'Classic',
   calcC: (inst: CardInstance, allPlayerCards: Array<Array<CardInstance>>) => {
     const colourlessCards: Array<CardInstance> = allPlayerCards.reduce(
       (allCards, playerCards) => {
         const clessCards: Array<CardInstance> = playerCards.filter(
-          (inst: CardInstance) => inst.card.type === 'colourless'
+          (inst: CardInstance) => inst.card.type.includes('colourless')
         );
         return [...clessCards, ...allCards];
       },
@@ -26,7 +26,7 @@ addCard(aiTakeover);
 
 const bio_plague: CatastopheCard = {
   name: 'BIOENGINEERED PLAGUE',
-  type: 'catastrophe',
+  type: ['catastrophe'],
   pack: 'Techlings',
   calcC: (inst: CardInstance, allPlayerCards: Array<Array<CardInstance>>) => {
   // remove a card (set score to 0) from each players hand
@@ -67,6 +67,14 @@ addCard(bio_plague);
 // TODO
 // addBasicCard('EYES OPEN FROM BEHIND THE STARS', 'purple', 'Mythlings', );
 
+// TODO
+// addBasicCard('DENIAL', 'colourless', 'Classic', 4);
 
-// TODO Catastrophe
+// TODO
 // addBasicCard('DEUS EX MACHINA', 'green', 'Classic', );
+
+// TODO
+// addBasicCard('GLACIAL MELTDOWN', 'blue', 'Classic', );
+
+// TODO 
+// addBasicCard('GREY GOO', 'colourless', 'Classic', );
--- src/cards/d-cards.ts
+++ src/cards/d-cards.ts
@@ -2,49 +2,31 @@ import { addBasicCard, addCard } from "../cardContainer";
 import { CardInstance, CardType, PlayerCard } from "../types";
 
 addBasicCard('DEEP ROOTS', 'green', 'Classic', 2);
-
-const delicious: PlayerCard = {
-  name: 'DELICIOUS',
-  type: 'colourless',
-  pack: 'Classic',
-  calcA: function (card: CardInstance, allPlayerCards: CardInstance[][], currentPlayer: number): void {
-    const currentPlayerCards = allPlayerCards[currentPlayer];
-    // A different colourless card!
-    const colourlessTraitCard = currentPlayerCards.find(card => card.card.type === 'colourless' && this.name !== card.card.name);
-    if (colourlessTraitCard) {
-        card.finalA = 4;
-    } else {
-        card.finalA = 0;
-    }
-  }
-};
-addCard(delicious);
-
-// TODO: Denial has been skipped as it's catastrophe. related
-// addBasicCard('DENIAL', 'colourless', 'Classic', 4);
-
+addBasicCard('DELICIOUS', 'colourless', 'Classic', 4);
 addBasicCard('DERMAL ARMOR', 'colourless', 'Dinolings', 2);
 addBasicCard('DESTINED', 'colourless', 'Mythlings', 4);
 addBasicCard('DIAPHANOUS WINGS', 'blue', 'Mythlings', -1);
 addBasicCard('DIRECTLY REGISTER', 'purple', 'Classic', 1);
 addBasicCard('DOTING', 'colourless', 'Classic', 2);
 
-// 4 points if "all 4 colours" are present
+// Bonus 4 points if "all 4 colours" are present
 const dragonHeart: PlayerCard = {
   name: 'DRAGON HEART',
-  type: 'red',
+  type: ['red'],
   pack: 'Mythlings',
-  calcA: function (card: CardInstance, allPlayerCards: CardInstance[][], currentPlayer: number): void {
-    const currentPlayerCards = allPlayerCards[currentPlayer];
+  calcA: function (card: CardInstance): void {
+    card.finalA = 1;
+  }, 
+  calcB: function (card: CardInstance, allPlayerCards, currentPlayer: number) : void {
+     const currentPlayerCards = allPlayerCards[currentPlayer];
     
-    const validColours = ['purple', 'green', 'red', 'blue'] as const;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const uniqueMatchingColours = new Set(currentPlayerCards.filter(c => validColours.includes(c.card.type as unknown as any) ).map(c => c.card.type));
+    const validColours: readonly CardType[] = ['purple', 'green', 'red', 'blue'] as const;
+    const uniqueMatchingColours: Set<CardType> = new Set(currentPlayerCards.filter(c => validColours.find(colour => c.card.type.includes(colour))).map(c => c.card.type).flat());
 
     if (uniqueMatchingColours.size === validColours.length) {
-      card.finalA = 5;
+      card.finalB = 4;
     } else {
-      card.finalA = 1;
+      card.finalB = 0;
     }
   }
 };
